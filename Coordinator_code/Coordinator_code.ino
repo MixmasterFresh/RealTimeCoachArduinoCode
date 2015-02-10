28d5c76586672e435db72bd0e1a19932c99b1259
Austin Horning <austinseven@outlook.com>
2015-02-10 15:13:49 -0500

    Coordinator Finished... I think...
    Could be not finished... or finishedless... or something...

--- Coordinator_code/Coordinator_code.ino
+++ Coordinator_code/Coordinator_code.ino
@@ -4,44 +4,57 @@ int state = 0;
 String data = ""; 
 SoftwareSerial portTwo(8,9);
 boolean check=true;
-String command = "";
-char assembler[20];
+char command = 'a';
+char assembler[50];
 int counter=0;
 
 
 void setup() {
- Serial.begin(9600); // Default connection rate for my BT module
- portTwo.begin(9600);
- Serial.write("+++");
- while(check) {
-   if(Serial.available()>0){
-   char[ = Serial.read();
-   }
-   if(data=="OK") {
-     check=false;
-   }
- }
- Serial.write("ATID4567");
- delay(50);
- Serial.write("ATCH0x0D");
- delay(50);
- Serial.write("ATMY0");
- delay(50);
- Serial.write("CN");
- delay(1000);
+    Serial.begin(9600);
+    portTwo.begin(9600);
+    Serial.write("+++");
+        while(check) {
+            if(Serial.available()>0){
+                assembler[counter] = Serial.read();
+                data=data+assembler[counter];
+                counter++;
+                if(counter==2){
+                    break;
+                }
+            }
+       
+         if(data=="OK") {
+             check=false;
+         }
+     }
+     Serial.write("ATID4567");
+     delay(50);
+     Serial.write("ATCH0D");
+     delay(50);
+     Serial.write("ATMY0");
+     delay(50);
+     Serial.write("CN");
+     delay(1000);
 }
  
 void loop() {
- Serial.listen();
- while(
-   if(Serial.available() > 0){
-     data = Serial.read();
+   counter=0;
+   while(true) {
+       if(Serial.available() > 0) {
+           assembler[counter] = Serial.read();
+           if (assembler[counter]=='?') {
+               break;
+           }
+           counter++;
+       }
    }
- while() {
- portTwo.write(data)
- portTwo.listen();
- if(portTwo.available() > 0){
-   command = portTwo.read();
- }
-}
 
+    while(counter>=0){
+        portTwo.write(assembler[counter]);
+        counter--;
+    }   
+    portTwo.listen();
+    if(portTwo.available() > 0) {
+        command = portTwo.read();
+    }
+}
