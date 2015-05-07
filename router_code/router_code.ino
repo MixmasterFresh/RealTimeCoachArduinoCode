5e1f92f25b154f1d9ec2fa2301c216d513bdc9c8
Austin Seven Horning <austinseven@outlook.com>
2015-05-07 15:14:16 -0400

    final upload

--- router_code/router_code.ino
+++ router_code/router_code.ino
@@ -2,13 +2,15 @@
 int ledPin = 13; 
 int state = 0;
 String data = "";
-SoftwareSerial portOne(0,1);
+SoftwareSerial portOne(11,12);
 SoftwareSerial portTwo(8,9);
-boolean check=true;
+int check=0;
 char command = 'a';
 char assembler[50];
 int counter=0;
 byte message;
+byte message1;
+byte message2;
 int addresses[50];
 unsigned long time;   
 unsigned long breaker; 
@@ -20,24 +22,29 @@ void setup() {
 }
  
 void loop() {
+    time = millis();
     portTwo.listen();
-    check = true;
-    while(check) {
+    check = 0;
+    while(check<2) {
         if(portTwo.available() > 0) {
-            message =portTwo.read();
+            message = portTwo.read();
             portOne.write(message);
-            portOne.write('-1');
-            check = false;
+            check++;
         }
-    }  
+    }
     portOne.listen();
+    writeIntAsBinary(-1);  
     state=0;
-    while(state<20) {
+    breaker = time;
+    while(state<20 && time - breaker <1000) {
         if(portOne.available() > 0) {
             message = portOne.read();
             portTwo.write(message);
             state++;
         }
     }
-    
+}
+void writeIntAsBinary(int value){
+    portOne.write(lowByte(value));
+    portOne.write(highByte(value));
 }
--- senderCode/senderCode.ino
+++ senderCode/senderCode.ino
@@ -29,27 +29,42 @@ int bx = 0;
 int by = 0;
 int bz = 0;
 
+int hxrest = 0;
+int hyrest = 0;
+int hzrest = 0;
+int bxrest = 0;
+int byrest = 0;
+int bzrest = 0;
+
 int lastinput = 0;
 
-int check[]= {0,0};
+boolean right = true;
+
+byte check_i[]= {0,0,0,0};
+int check[] ={0,0};
 
 int period[4]={1.0,1.0,1.0,1.0};
 
 void setup() {
     pinMode(heart_signal, INPUT);
     Serial.begin(9600);
-    digitalWrite(13,HIGH);
+    int hxrest = analogRead(headx_signal);
+    int hyrest = analogRead(heady_signal);
+    int hzrest = analogRead(headz_signal);
+    int bxrest = analogRead(bodyx_signal);
+    int byrest = analogRead(bodyy_signal);
+    int bzrest = analogRead(bodyz_signal);
 }
 
 void loop() {
     time = millis();
     heart_input = digitalRead(heart_signal);
-    headx_input = analogRead(headx_signal);
-    heady_input = analogRead(heady_signal);
-    headz_input = analogRead(headz_signal);
-    bodyx_input = analogRead(bodyx_signal);
-    bodyy_input = analogRead(bodyy_signal);
-    bodyz_input = analogRead(bodyz_signal);
+    headx_input = analogRead(headx_signal) - hxrest;
+    heady_input = analogRead(heady_signal) - hyrest;
+    headz_input = analogRead(headz_signal) - hzrest;
+    bodyx_input = analogRead(bodyx_signal) - bxrest;
+    bodyy_input = analogRead(bodyy_signal) - byrest;
+    bodyz_input = analogRead(bodyz_signal) - bzrest;
 
     if ((heart_input != lastinput)&&(heart_input == HIGH)) {
         // If the pin state has just changed from low to high (edge detector)
@@ -72,26 +87,53 @@ void loop() {
         by = bodyy_input;
         bz = bodyz_input;
     }
-    if(Serial.available() > 0) {
-        check[1] = check[0];
-        check[0] = Serial.read();
-        if(check[1] == id && check[0] == -1){
-            Serial.write(hx);
-            Serial.write(hy);
-            Serial.write(hz);
-            Serial.write(bx);
-            Serial.write(by);
-            Serial.write(bz);
-            Serial.write(period[0]);
-            Serial.write(period[1]);
-            Serial.write(period[2]);
-            Serial.write(period[3]);
+    if(time-starttime>5000) {
+        period[3] = 256;
+        period[2] = 256;
+        period[1] = 256;
+        period[0] = 256;
+    }
+    while(Serial.available() > 0) {
+        check_i[3] = check_i[2];
+        check_i[2] = check_i[1];
+        check_i[1] = check_i[0];
+        check_i[0] = Serial.read();
+        right = !right;
+        if(right){
+            check[1] = check[0];
+            check[0] = Combine(check_i[1],check_i[0]);
+        }
+        if(check[1] == id && check[0] == 5000){
+            writeIntAsBinary(hx);
+            writeIntAsBinary(hy);
+            writeIntAsBinary(hz);
+            writeIntAsBinary(bx);
+            writeIntAsBinary(by);
+            writeIntAsBinary(bz);
+            writeIntAsBinary(period[0]);
+            writeIntAsBinary(period[1]);
+            writeIntAsBinary(period[2]);
+            writeIntAsBinary(period[3]);
             hx = 0;
             hy = 0;
             hz = 0;
             bx = 0;
             by = 0;
             bz = 0;
+            
         }
     }
+}
+int Combine( unsigned char x_high, unsigned char x_low)
+{
+  int combined; 
+  combined = x_high;              //send x_high to rightmost 8 bits
+  combined = combined<<8;         //shift x_high over to leftmost 8 bits
+  combined |= x_low;                 //logical OR keeps x_high intact in combined and fills in                                                             //rightmost 8 bits
+  return combined;
+}
+void writeIntAsBinary(int value){
+    Serial.write(lowByte(value));
+    Serial.write(highByte(value));
+    
 }
